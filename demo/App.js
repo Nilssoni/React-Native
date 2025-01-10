562506b0b3e7c86c50db1a6eb1043b1d5797b5a6
Nilssoni <martikainen93@gmail.com>
2025-01-10 10:58:21 +0200

    Demo app test

--- demo/App.js
+++ demo/App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [miles, setMiles] = useState('');
+
+  const kilometers = miles * 1.609;
+
   return (
     <View style={styles.container}>
-      <Text>This is a test, please ignore!</Text>
+      <Text>Miles</Text>
+      <TextInput
+        value={miles}
+        onChangeText={text => setMiles(text)}
+        keyboardType='decimal-pad'
+      />
+      <Text>Kilometers</Text>
+      <Text>{kilometers}</Text>
       <StatusBar style="auto" />
     </View>
   );
